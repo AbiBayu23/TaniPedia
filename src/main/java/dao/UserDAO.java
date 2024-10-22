ad16ee307765baeb8d69766f71fbc5d423d036b2
AbiBayu23 <148162535+AbiBayu23@users.noreply.github.com>
2024-10-22 20:19:31 +0700

    Update UserDAO.java

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -9,14 +9,13 @@ public class UserDAO {
 
     public boolean registerUser(UserModel user) {
         try (Connection con = BaseDAO.getCon()) {
-            String query = "INSERT INTO user (password_user, nama_user, email_user, nomor_hp, alamat_user, role) VALUES (?, ?, ?, ?, ?, ?)";
+            String query = "INSERT INTO user (nama_user, email_user, password_user,  nomor_hp, Role) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1, user.getPasswordUser());
-            ps.setString(2, user.getNamaUser());
-            ps.setString(3, user.getEmailUser());
+            ps.setString(1, user.getNamaUser());
+            ps.setString(2, user.getEmailUser());
+            ps.setString(3, user.getPasswordUser());
             ps.setString(4, user.getNomorHp());
-            ps.setString(5, user.getAlamatUser());
-            ps.setString(6, user.getRole());
+            ps.setString(5, user.getRole());
 
             int result = ps.executeUpdate();
             return result > 0;
@@ -38,12 +37,11 @@ public class UserDAO {
             if (rs.next()) {
                 user = new UserModel();
                 user.setIdUser(rs.getInt("id_user"));
-                user.setPasswordUser(rs.getString("password_user"));
                 user.setNamaUser(rs.getString("nama_user"));
                 user.setEmailUser(rs.getString("email_user"));
+                user.setPasswordUser(rs.getString("password_user"));
                 user.setNomorHp(rs.getString("nomor_hp"));
-                user.setAlamatUser(rs.getString("alamat_user"));
-                user.setRole(rs.getString("role"));
+                user.setRole(rs.getString("Role"));
             }
         } catch (Exception e) {
             e.printStackTrace();
