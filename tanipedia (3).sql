2de105ab2f4dc169e8855f32ea35196d65ecb046
AbiBayu23 <148162535+AbiBayu23@users.noreply.github.com>
2024-10-23 08:52:01 +0700

    Add files via upload

---
+++ tanipedia (3).sql
@@ -0,0 +1,299 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Okt 2024 pada 03.51
+-- Versi server: 8.0.36
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tanipedia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail_transaksi` int NOT NULL,
+  `id_order` int NOT NULL,
+  `kuantitas` int NOT NULL,
+  `produk` varchar(16) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ensiklopedia`
+--
+
+CREATE TABLE `ensiklopedia` (
+  `id_ensiklopedia` int NOT NULL,
+  `nama_ensiklopedia` int NOT NULL,
+  `id_tanaman` int NOT NULL,
+  `jenis_ensiklopedia` enum('Panduan Budidaya','Penaganan Penyakit dan Hama','Pupuk yang cocok','') NOT NULL,
+  `konten` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hama_penyakit`
+--
+
+CREATE TABLE `hama_penyakit` (
+  `id_hama_penyakit` int NOT NULL,
+  `nama_hama_penyakit` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kamus`
+--
+
+CREATE TABLE `kamus` (
+  `id_kamus` int NOT NULL,
+  `istilah` varchar(16) NOT NULL,
+  `konten` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order`
+--
+
+CREATE TABLE `order` (
+  `id_order` int NOT NULL,
+  `id_user` int NOT NULL,
+  `Total` int NOT NULL,
+  `Tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penanganan_penyakit`
+--
+
+CREATE TABLE `penanganan_penyakit` (
+  `id_penanganan_penyakit` int NOT NULL,
+  `id_tanaman` int NOT NULL,
+  `id_hama_penyakit` int NOT NULL,
+  `id_pestisida` int NOT NULL,
+  `cara_penanganan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pestisida`
+--
+
+CREATE TABLE `pestisida` (
+  `id_pestisida` int NOT NULL,
+  `nama_pestisida` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `harga_pestisida` int DEFAULT NULL,
+  `jenis_pestisida` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pupuk`
+--
+
+CREATE TABLE `pupuk` (
+  `id_pupuk` int NOT NULL,
+  `nama_pupuk` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `harga_pupuk` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tanaman`
+--
+
+CREATE TABLE `tanaman` (
+  `id_tanaman` int NOT NULL,
+  `nama_tanaman` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `jenis_tanaman` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL,
+  `password_user` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Role` enum('admin','customer') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nama_user` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email_user` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nomor_hp` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `password_user`, `Role`, `nama_user`, `email_user`, `nomor_hp`) VALUES
+(1, 'perkasa23@rcm', 'admin', 'abi_bayu', 'abibayu83@gmail.com', '081222965873');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail_transaksi`),
+  ADD KEY `id_order` (`id_order`);
+
+--
+-- Indeks untuk tabel `ensiklopedia`
+--
+ALTER TABLE `ensiklopedia`
+  ADD PRIMARY KEY (`id_ensiklopedia`),
+  ADD KEY `id_tanaman` (`id_tanaman`);
+
+--
+-- Indeks untuk tabel `hama_penyakit`
+--
+ALTER TABLE `hama_penyakit`
+  ADD PRIMARY KEY (`id_hama_penyakit`);
+
+--
+-- Indeks untuk tabel `kamus`
+--
+ALTER TABLE `kamus`
+  ADD PRIMARY KEY (`id_kamus`);
+
+--
+-- Indeks untuk tabel `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `penanganan_penyakit`
+--
+ALTER TABLE `penanganan_penyakit`
+  ADD PRIMARY KEY (`id_penanganan_penyakit`),
+  ADD KEY `id_penanganan_penyakit_2` (`id_penanganan_penyakit`,`id_hama_penyakit`),
+  ADD KEY `id_penanganan_penyakit_3` (`id_penanganan_penyakit`,`id_pestisida`),
+  ADD KEY `id_tanaman` (`id_tanaman`);
+
+--
+-- Indeks untuk tabel `pestisida`
+--
+ALTER TABLE `pestisida`
+  ADD PRIMARY KEY (`id_pestisida`);
+
+--
+-- Indeks untuk tabel `pupuk`
+--
+ALTER TABLE `pupuk`
+  ADD PRIMARY KEY (`id_pupuk`);
+
+--
+-- Indeks untuk tabel `tanaman`
+--
+ALTER TABLE `tanaman`
+  ADD PRIMARY KEY (`id_tanaman`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail_transaksi` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `ensiklopedia`
+--
+ALTER TABLE `ensiklopedia`
+  MODIFY `id_tanaman` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `hama_penyakit`
+--
+ALTER TABLE `hama_penyakit`
+  MODIFY `id_hama_penyakit` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kamus`
+--
+ALTER TABLE `kamus`
+  MODIFY `id_kamus` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `order`
+--
+ALTER TABLE `order`
+  MODIFY `id_order` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penanganan_penyakit`
+--
+ALTER TABLE `penanganan_penyakit`
+  MODIFY `id_penanganan_penyakit` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pestisida`
+--
+ALTER TABLE `pestisida`
+  MODIFY `id_pestisida` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pupuk`
+--
+ALTER TABLE `pupuk`
+  MODIFY `id_pupuk` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tanaman`
+--
+ALTER TABLE `tanaman`
+  MODIFY `id_tanaman` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
