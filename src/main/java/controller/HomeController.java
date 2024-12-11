bbd5e08ad3c9d61f78373f28bfa047d2d8c86264
AbiBayu23 <148162535+AbiBayu23@users.noreply.github.com>
2024-12-11 14:32:19 +0700

    Update HomeController.java

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -4,9 +4,19 @@
  */
 package controller;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +25,17 @@ import javafx.fxml.Initializable;
  */
 public class HomeController implements Initializable {
 
+    @FXML
+    private Button Home;
+    @FXML
+    private Button Ensiklopedia;
+    @FXML
+    private Button Kamus;
+    @FXML
+    private Button Belanja;
+    @FXML
+    private ImageView Logout;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +43,18 @@ public class HomeController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+    public void Logout(MouseEvent event) {
+    try {
+        URL url = new File("src/main/java/view/Login.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Stage stage = (Stage) Logout.getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    } catch (Exception e) {
+        File file = new File("src/main/java/view/Login.fxml");
+        System.out.println("File exists: " + file.exists());
+    }
+}
 }
